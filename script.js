3dc2ddd5796a351685a1b7c65b072558db66d8f5
Antony Dias <106544295+tonao7777@users.noreply.github.com>
2023-09-30 20:39:43 -0300

    Jogo de Adivinha Em Dupla
    Coisas simples

---
+++ script.js
@@ -0,0 +1,34 @@
+// Gera um número aleatório entre 1 e 100
+const numeroAleatorio = Math.floor(Math.random() * 50) + 7;
+let tentativas = 5;
+
+
+
+function verificarAdivinhacao() {
+    // Obtém a tentativa do jogador
+    const tentativa = document.getElementById("tentativa").value;
+    tentativas++;
+
+    
+
+    // Compara a tentativa com o número aleatório
+    if (tentativa == numeroAleatorio) {
+        document.getElementById("mensagem").innerHTML = `Parabéns! O Vencedor é o número ${numeroAleatorio}.`;
+        document.getElementById("mensagem").style.color = "green";
+        document.getElementById("tentativa").disabled = true;
+    } else if (tentativa < numeroAleatorio) {
+        document.getElementById("mensagem").innerHTML = "Tente um número maior.";
+        document.getElementById("mensagem").style.color = "red";
+
+    } else {
+        document.getElementById("mensagem").innerHTML = "Tente um número menor.";
+        document.getElementById("mensagem").style.color = "red";
+    }
+}
+
+alert("Iremos Jogar O Jogo da Adivinhação Em Dupla")
+alert("Se Decidam quem será o 1 e o 2"); 
+alert("Cada Jogada Serve Reversar entre si")
+alert("Boa Sorte a vocês"); 
+  
+ 
